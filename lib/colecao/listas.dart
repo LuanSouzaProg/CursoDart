c9bb0699970fa28a373fe5a3a9b55235c24d46c9
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2022-02-10 11:58:28 -0300

    CursoDart

---
+++ lib/colecao/listas.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  List<String> nomes = [
+    "Luan",
+    "Arthur",
+    "Natan",
+    "Gustavo"
+  ];
+
+  print(nomes);
+
+  List<int> idades = [
+    20,
+    21,
+    35,
+    58
+  ];
+
+  print(idades);
+  print(idades.length);
+  print(idades.first);
+  print(idades.last);
+  print(idades[1]);
+
+
+}
